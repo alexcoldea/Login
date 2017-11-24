46ea0358937faeb2f0de547351bc6f6f128b82b5
mathlah <christopherlee229@gmail.com>
2017-11-24 19:16:34 +0000

    Added Gallery class.

---
+++ Assignment 3 group 6/src/application/Gallery.java
@@ -0,0 +1,16 @@
+package application;
+/**
+ * This class represents a browsing mechanism for artworks. 
+ * @author mathlah
+ * @version 1.0
+ */
+
+public class Gallery {
+	
+	public Gallery() {
+		
+		while (true) {
+			System.out.println("Work in progress");
+		}
+	}
+}
