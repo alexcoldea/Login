2b475346b62b635d79894ec5c1334474d80b11e1
Christopher Lee <christopherlee229@gmail.com>
2017-11-26 17:02:41 +0000

    Created load class. Added basic methods to read the file.

---
+++ Assignment 3 group 6/src/application/Load.java
@@ -0,0 +1,71 @@
+package application;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+/**
+ * This class is responsible for loading entities such as artworks and profiles.
+ * @author Christopher Lee
+ * @version 1.0
+ */
+
+public class Load {
+	//TODO load artworks, load profiles, 
+	
+	/**
+	 * Method to read the file of artworks and returns a list of artworks. The method
+	 * should exit the program if an error has occurred.
+	 * @return The list of artworks in the file.
+	 */
+	public static ArrayList<Artwork> loadArtworks() {
+		Scanner in = null;
+		
+		File file = new File("artwork.txt");
+		try {
+			in = new Scanner(file);
+		} catch (Exception e) {
+			System.out.println("Unable to open artwork.txt");
+			System.exit(1);
+		}
+		
+		return readArtworks(in);
+	}
+	
+	private static ArrayList<Artwork> readArtworks(Scanner in) {
+		ArrayList<Artwork> artworks = new ArrayList<>();
+		String artworkType = "";
+		String line = "";
+		
+		while (in.hasNextLine()) {
+			try {
+				artworkType = in.next();
+			} catch (Exception e) {
+				artworkType = "empty";
+			}
+			
+			switch (artworkType) {
+				case "painting":
+					line = in.nextLine();
+					artworks.add(createPainting(line));
+					break;
+				case "sculpture":
+					line = in.nextLine();
+					artworks.add(createSculpture(line));
+					break;
+				default:
+					break;
+			}
+		}
+		return null;
+	}
+	
+	private static Painting createPainting(String line) {
+		return null;
+	}
+	
+	private static Sculpture createSculpture(String line) {
+		return null;
+	}
+}
